23798409b40ab12cdd40be0fd1650233ac3d3984
Børre Gaup <albbas@gmail.com>
2026-01-29 12:19:17 +0100

    Make tests pass

--- recorder-ui/src/features/configuration/__mocks__/ConfigurationService.ts
+++ recorder-ui/src/features/configuration/__mocks__/ConfigurationService.ts
@@ -1,6 +1,6 @@
-import testSchedule from "../test-assets/231ab28d-a442-4a78-8efb-9220bf01e933.json";
+import summerSchedule from "../test-assets/summer-schedule.json";
 export const getConfigurations = jest.fn(async () => {});
 
 export const getConfiguration = jest.fn(async (id: string) =>
-  Promise.resolve(testSchedule)
+  Promise.resolve(summerSchedule)
 );
